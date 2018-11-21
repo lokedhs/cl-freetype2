fe9d8108154cb4f4e46622bcaea4ba6155a3b5af
Elias Mårtenson <lokedhs@gmail.com>
2018-11-21 15:26:54 +0800

    Explicitly try to load libfreetype.so.6

--- src/ffi/ft2-lib.lisp
+++ src/ffi/ft2-lib.lisp
@@ -5,6 +5,7 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (define-foreign-library freetype2
     (:windows (:or "libfreetype.dll" "libfreetype-6.dll"))
+    (:linux (:or "libfreetype.so.6" "libfreetype"))
     (t (:default "libfreetype")))
 
   (use-foreign-library freetype2))
